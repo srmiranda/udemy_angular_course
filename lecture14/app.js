a7e69a79ad9bba2f85c76981f4cecb0920a024f5
SasquatchShawn <srmiranda1@gmail.com>
2015-12-13 11:07:13 -0500

    Lesson done

--- lecture14/app.js
+++ lecture14/app.js
@@ -1,5 +1,11 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($scope) {
+myApp.controller('mainController', function($scope, $log, $filter, $resource) {
+    
+    $scope.name = 'John';
+    $scope.formattedName = $filter('uppercase')($scope.name);
+    
+    $log.info($scope.name);
+    $log.info($scope.formattedName);
     
 });
