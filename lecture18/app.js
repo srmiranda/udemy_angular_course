cdd88239ba5b75d46d0924ec8753d50a8c93ee2d
SasquatchShawn <srmiranda1@gmail.com>
2015-12-13 21:33:18 -0500

    lesson done

--- lecture18/app.js
+++ lecture18/app.js
@@ -1,7 +1,11 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
     
+    $scope.handle = '';
     
+    $scope.lowerCaseHandle = function() {
+        return $filter('lowercase')($scope.handle);
+    }
     
 }]);
