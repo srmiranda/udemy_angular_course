205745c91dd0ad6de2282f8c805808bbcd892440
SasquatchShawn <srmiranda1@gmail.com>
2015-12-15 22:11:54 -0500

    lesson done

--- lecture18/app.js
+++ lecture18/app.js
@@ -1,3 +1,4 @@
+
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
--- lecture20/app.js
+++ lecture20/app.js
@@ -1,6 +1,26 @@
-var tb = document.getElementById("name");
+var myApp = angular.module('myApp', []);
 
-tb.addEventListener("keypress",
-    function(event) {
-        console.log("Pressed!");
+myApp.controller('mainController', ['$scope', '$filter', '$timeout', function($scope, $filter, $timeout) {
+    
+    $scope.handle = '';
+    
+    $scope.lowerCaseHandle = function() {
+        return $filter('lowercase')($scope.handle);
+    };
+    
+    $scope.$watch('handle', function(newValue, oldValue) {
+        
+       console.info('Changed!');
+       console.log('Old: ' + oldValue);
+       console.log('New: ' + newValue);
+        
     });
+    
+    $timeout(function() {
+       
+       $scope.handle = 'newTwitterHandle';
+       console.log('Scope changed!');
+        
+    }, 3000);
+    
+}]);
