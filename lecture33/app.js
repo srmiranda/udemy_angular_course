376e2dc365588645b203bf389792d70125529c43
SasquatchShawn <srmiranda1@gmail.com>
2015-12-19 21:27:54 -0500

    lesson 40 done

--- lecture33/app.js
+++ lecture33/app.js
@@ -23,13 +23,28 @@ myApp.config(function ($routeProvider) {
 
 myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
     
-    $scope.person = {
+    $scope.people = [
+        {
         name: 'John Doe',
         address: '555 Main St.',
         city: 'New York',
         state: 'NY',
-        zip: '1111'
-    }
+        zip: '11111'
+        },
+        {
+        name: 'Jane Doe',
+        address: '333 Second St.',
+        city: 'Buffalo',
+        state: 'NY',
+        zip: '22222'
+        },
+        {
+        name: 'George Doe',
+        address: '111 Third St.',
+        city: 'Miami',
+        state: 'FL',
+        zip: '33333'
+        }]
     
     $scope.formattedAddress = function(person) {
         return person.address + ', ' + person.city + ' ' + person.state + ' ' + person.zip;
@@ -61,7 +76,22 @@ myApp.directive("searchResult", function() {
            // = sign means Two Way Binding for Object
            //   changes in directive affect main object
            // @ sign means Text - One Way Binding
-       }
+       },
+       link: function(scope, elements, attrs) {
+                   
+                   console.log('Linking...');
+                   
+                   console.log(scope);
+                   
+                   if (scope.personObject.name == 'Jane Doe') {
+                       elements.removeAttr('class');
+                   }
+                   
+                   console.log(elements);
+        }
+       // Careful about length of data when doing this
+       
+       
    } 
     
 });
